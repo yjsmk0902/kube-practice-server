07e7239902d1ecc3515567c2ea5d624341b3aa16
Luke_YS <yjsmk0902@gmail.com>
2025-03-24 18:21:35 +0900

    init: set up project

---
+++ src/main/java/com/practice/kube_practice_server/KubePracticeServerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.kube_practice_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubePracticeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubePracticeServerApplication.class, args);
+	}
+
+}
