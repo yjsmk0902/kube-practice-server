bfc7d85a00cf4bbb8326323a86962ba5d736b7da
Luke_YS <yjsmk0902@gmail.com>
2025-03-31 09:42:56 +0900

    init: set health check

---
+++ src/main/java/com/practice/kube_practice_server/global/common/presentation/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.practice.kube_practice_server.global.common.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@GetMapping("/heath-check")
+	public String healthCheck() {
+		return "OK";
+	}
+
+}
